f865e2fed0bf61d918d3d78fce03b6023d2a1c66
proochster <proochster@gmail.com>
2021-07-20 20:09:47 +0100

    ^ WorkBox

--- _includes/js/service-worker.js
+++ _includes/js/service-worker.js
@@ -26,6 +26,30 @@ self.addEventListener('message', (event) => {
  */
 self.__precacheManifest = [
   {
+    "url": "assets/icon-144x144.png",
+    "revision": "fb2cad437741e1136cbfd0a902147aa1"
+  },
+  {
+    "url": "assets/icon-16x16.png",
+    "revision": "2a4d763e3ec289c1167d9244ac9a4057"
+  },
+  {
+    "url": "assets/icon-32x32.png",
+    "revision": "6ecf539aab1606eb08a3ed7f6b71ec54"
+  },
+  {
+    "url": "assets/icon-512x512.png",
+    "revision": "a96453da12f6bec0e0bc6e4308e5168e"
+  },
+  {
+    "url": "assets/icon-64x64.png",
+    "revision": "344744435451c4f9dcd1265f47a8563d"
+  },
+  {
+    "url": "favicon.png",
+    "revision": "17e04db89dcff9516a446038e754eac3"
+  },
+  {
     "url": "main.js",
     "revision": "1b5eea5150f30b5d88872eca3f79e3cf"
   },
